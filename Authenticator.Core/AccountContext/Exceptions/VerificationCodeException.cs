e92af86861964710947b8111b615656e4f966dc0
Luan Vinicius Tavares Bregunce <luan.bregunce@rmmais.com.br>
2024-01-31 13:30:40 -0300

    upd: generation and comparison of verification code

---
+++ Authenticator.Core/AccountContext/Exceptions/VerificationCodeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authenticator.Core.AccountContext.Exceptions;
+public class VerificationCodeException : Exception
+{
+    public VerificationCodeException(string? message) : base(message)
+    {
+    }
+}
+
---
+++ Authenticator.Core/AccountContext/ValueObjects/VerificationCode.cs
@@ -0,0 +1,27 @@
+﻿using Authenticator.Core.AccountContext.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authenticator.Core.AccountContext.ValueObjects;
+public class VerificationCode
+{
+    public string Code { get; } = Guid.NewGuid().ToString("N")[..8];
+    public DateTime? ExpiresAt { get; private set; } = DateTime.UtcNow.AddMinutes(5);
+    public DateTime? VerifiedAt { get; private set; } = null;
+    public bool IsActived => VerifiedAt != null && ExpiresAt == null;
+
+    public void Verify(string code)
+    {
+        if (IsActived) throw new VerificationCodeException("This code is already verified");
+        if (VerifiedAt < DateTime.UtcNow) throw new VerificationCodeException("This code is expired");
+        if (string.Equals(code.Trim(),this.Code.Trim(),StringComparison.CurrentCulture)) throw new VerificationCodeException("The code typed is wrong");
+        ExpiresAt = null;
+        VerifiedAt =  DateTime.UtcNow;
+    }
+
+}
+
+
