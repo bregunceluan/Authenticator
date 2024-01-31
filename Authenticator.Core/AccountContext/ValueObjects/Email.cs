772f0526437d2cf21f5e76e48770dcb0aca7c0f2
Luan Bregunce <luan.bregunce@rmmais.com.br>
2024-01-30 21:49:21 -0300

    Add project files.

---
+++ Authenticator.Core/AccountContext/ValueObjects/Email.cs
@@ -0,0 +1,19 @@
+﻿using Authenticator.Core.SharedContext.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authenticator.Core.AccountContext.ValueObjects;
+
+public class Email
+{
+    public const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+    public string Address { get; }
+
+    public string Hash => Address.ToBase64(); 
+
+}
+
---
+++ Authenticator.Infra/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Authenticator.Infra;
+
+public class Class1
+{
+
+}
